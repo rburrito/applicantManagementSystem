449be4ab93fa217fb6435bc62b6db3f72f2ec7fa
Rita Law <12490147+rburrito@users.noreply.github.com>
2023-07-06 11:12:54 -0400

    please come push event

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(basicAuth({
   users:{
     'admin': 'scarySecretPasswordChanged133333',
     'adam':'revealedPassword1333333444444',
-    'eve':'dangerouslyEasyPassword13333'
+    'eve':'dangerouslyEasyPassword133334444'
   },
   challenge: true,
   realm:'Imb4T3st4pp'
