dd750eebb839a53320770fe4923aef6894d746a1
Rita Law <12490147+rburrito@users.noreply.github.com>
2023-09-18 15:26:24 -0400

    new index.js

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.use(session({
 
 app.use(basicAuth({
   users:{
-    'admin': 'scarySecretPasswordChanged0123456789',
-    'adam':'revealedPassword0123456789',
-    'eve':'dangerouslyDifficultPassword0123456789'
+    'admin': 'scarySecretPasswordChanged54321',
+    'adam':'revealedPassword54321',
+    'eve':'dangerouslyDifficultPassword54321'
   },
   challenge: true,
   realm:'Imb4T3st4pp'
